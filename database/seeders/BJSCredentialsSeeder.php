c8757ddba130100df83d50ea4260603f2b3dbe77
Muhamad Iqbal <mirfmaster@gmail.com>
2026-01-16 18:36:17 +0700

    fix: database issue

--- database/seeders/BJSCredentialsSeeder.php
+++ database/seeders/BJSCredentialsSeeder.php
@@ -2,15 +2,16 @@
 
 namespace Database\Seeders;
 
+use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Cache;
 
-class BJSCredentialsSeeder
+class BJSCredentialsSeeder extends Seeder
 {
     public function run(): void
     {
         Cache::put('bjs.credentials.username', config('bjs.username'));
         Cache::put('bjs.credentials.password', config('bjs.password'));
-        Cache::put('bjs.session.login_toggle', false);
+        Cache::put('bjs.session.login_toggle', true);
         Cache::put('bjs.session.failed_attempts', 0);
     }
 }
